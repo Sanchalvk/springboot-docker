f1209014eb3db8b2a4f9c4ac5f035678f24bf43e
Sanchalvk <khedkarsanchal86@gmail.com>
2025-04-19 05:10:09 -1100

    first commit

---
+++ demoJavaProject/src/main/java/springsimple_demo/demoJavaProject/DemoJavaProjectApplication.java
@@ -0,0 +1,14 @@
+package springsimple_demo.demoJavaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJavaProjectApplication.class, args);
+		 System.out.println("Hello, World!");
+	}
+
+}
